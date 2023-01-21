dd3050d460736adf5f84bfd6c0930380f4c93e73
Said Mounaim <saidmounaim00@gmail.com>
2023-01-21 12:39:26 +0100

    Create User functionallity, hashing password & add error middleware

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,7 +1,30 @@
 import { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
+import prisma from "../utils/prisma";
+import bcrypt from "bcryptjs";
 
 // @Desc Register User
 // @Route /api/user/register
 // @Method POST
-export const register = asyncHandler(async (req: Request, res: Response) => {});
+export const register = asyncHandler(async (req: Request, res: Response) => {
+  const { email, name, password } = req.body;
+
+  const userExist = await prisma.user.findUnique({ where: { email } });
+
+  if (userExist) {
+    res.status(401);
+    throw new Error("Email already exist");
+  }
+
+  const passwordHasing = bcrypt.hashSync(password, 8);
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: passwordHasing,
+    },
+  });
+
+  res.status(201).json(user);
+});
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes";
+import { notFound, errorHandler } from "./middlewares/error";
 
 dotenv.config();
 
@@ -10,6 +11,10 @@ app.use(express.json());
 
 // User Route
 app.use("/api/user", userRoutes);
+
+// Error Middleware
+app.use(errorHandler);
+app.use(notFound);
 
 const PORT = process.env.PORT || 5000;
 
